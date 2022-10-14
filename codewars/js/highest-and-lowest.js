cf57a964de1a19cdfd367509f01c94841bc1f5a0
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-14 15:29:15 -0500

    completed highest and lowest codewars problem

--- codewars/js/highest-and-lowest.js
+++ codewars/js/highest-and-lowest.js
@@ -5,10 +5,14 @@
 // }
 
 function highAndLow(listInput){
-    let numbers = listInput.split(' ').sort();
-    return numbers;
+    let numbersString = listInput.split(' ');
+    let numbersArray = numbersString.map(function(number){
+        return parseFloat(number);
+    })
+    numbersArray = numbersArray.sort(function(a, b){return a - b});
+    return numbersArray[numbersArray.length - 1].toString() + " " + numbersArray[0].toString();
     };
 
-
+numbersArray[0].toString()
 
 console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
