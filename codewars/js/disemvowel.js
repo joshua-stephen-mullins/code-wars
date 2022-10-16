1af4ee310f24b9b15f07d4826fd05d3bd616bde4
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-16 16:20:14 -0500

    completed codewars problem with a homemade replaceall

--- codewars/js/disemvowel.js
+++ codewars/js/disemvowel.js
@@ -1,13 +1,31 @@
 'use strict';
 
+// homemade replaceAll
+
+function fixedReplaceAll(string, search, replace) {
+    return string.split(search).join(replace);
+}
+
 function disemvowel(string){
     let vowels = ['a', 'e' , 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     let j = 0;
     for (let i = 0; i < 100; i++){
-            string = string.replace(vowels[i], '');
+        string = fixedReplaceAll(string, vowels[i], "");
         console.log(string);
     } return string;
 }
+
+// didnt work on codewars because of some nodejs issues... replaceAll doesnt work
+
+//
+// function disemvowel(string){
+//     let vowels = ['a', 'e' , 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+//     let j = 0;
+//     for (let i = 0; i < 100; i++){
+//             string = string.replace(vowels[i], '');
+//         console.log(string);
+//     } return string;
+// }
 
 
 console.log(disemvowel('This website is for losers LOL!'));
