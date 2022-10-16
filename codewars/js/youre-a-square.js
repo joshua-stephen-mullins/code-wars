9e22f7eb786e21f890f3ec592251752afe841f2c
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-16 18:38:23 -0500

    began youre a square codewars problem

---
+++ codewars/js/youre-a-square.js
@@ -0,0 +1,10 @@
+'use script';
+
+function isSquare(n){
+    if (n < 0){
+        return n + ': Negative numbers cannot be square numbers'
+    } else if (Math.sqrt(n) === )
+
+    Math.sqrt(n)
+    return false;
+}
